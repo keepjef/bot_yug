db4f7d97387b041d1e9cf231314a609c7452967a
JustDoges <markpilya@gmail.com>
2025-02-14 18:07:34 +0300

    -added not_sending.py (function of checking and sending notifications)

--- not_sending.py
+++ not_sending.py
@@ -1,19 +1,31 @@
 import datetime
-
+from datetime import datetime
+import calendar
+import asyncio
+import time
 data = {
-            "date": "07.02.2025",
-            "time": "18.04",
-            "text": "hi"
+            "date": input("Введите дату (В формате DD.MM.YY)"),
+            "time": input("Введите дату (В формате HH.MM)"),
+            "text": input("Введите текст упоминания")
 }
 
 
-def Check_Not():
-    while True:
-        ndate = f"{data["date"]} {data["time"]}"
-        dater = datetime.datetime.now().strftime('%d.%m.%Y %H.%M')
 
-        if ndate == dater:
-            print(data["text"])
-            break
+async def time_sleep():
+
+    ndate = f"{data["date"]} {data["time"]}".replace(".", "/")
+    element = datetime.strptime(ndate, "%d/%m/%Y %H/%M")
+    tuple = element.timetuple()
+    timestamp = int(time.mktime(tuple))
+    dt = time.mktime(datetime.today().timetuple())
+    sleept = timestamp - dt
+    print(sleept)
+    await asyncio.sleep(sleept)
+
+def Check_Not():
+    asyncio.run(time_sleep())
+    print(data["text"])
 
 Check_Not()
+
+
